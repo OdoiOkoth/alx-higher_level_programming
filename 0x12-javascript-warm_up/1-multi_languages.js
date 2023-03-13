d9adb10b4d75b7545701c6198b4764a8bdb77747
OdoiOkoth <odoiokoth@gmail.com>
2023-03-13 11:53:45 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-// It prints 3 lines
-
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(
+  elem => console.log(elem)
+);
